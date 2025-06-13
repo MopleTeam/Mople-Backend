ff8a37f06defae9c43778d2bb5d13c2bb52915ed
OhYeHyun <ohyhohyl@gmail.com>
2025-06-14 03:03:45 +0900

    feat: External Api 응답 로거

--- src/main/java/com/groupMeeting/global/enums/logging/LoggerMessage.java
+++ src/main/java/com/groupMeeting/global/enums/logging/LoggerMessage.java
@@ -7,6 +7,11 @@ public enum LoggerMessage {
     INTERNAL_API_FAIL("Internal API 응답 실패"),
     INTERNAL_API_FAIL_WITH_NO_EXCEPTION("Exception 없는 예외 발생"),
 
+    // external api 로그
+    EXTERNAL_API_SUCCESS("External API 응답 성공"),
+    EXTERNAL_API_SLOW_RESPONSE("External API 느린 응답 감지"),
+    EXTERNAL_API_FAIL("External API 응답 실패"),
+
     // exception 로그
     EXCEPTION_CLIENT("Exception 클라이언트 오류 발생"),
     EXCEPTION_SERVER("Exception 서버 오류 발생");
---
+++ src/main/java/com/groupMeeting/global/logging/logger/ExternalApiLogger.java
@@ -0,0 +1,38 @@
+package com.groupMeeting.global.logging.logger;
+
+import com.groupMeeting.global.logging.LoggerAdapter;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import static com.groupMeeting.global.enums.logging.LoggerKey.*;
+import static com.groupMeeting.global.enums.logging.LoggerMessage.*;
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+
+@Slf4j
+@Component
+public class ExternalApiLogger implements LoggerAdapter {
+    private static final Logger externalApiLogger = LoggerFactory.getLogger("external-api");
+
+    @Override
+    public void logComplete(long executionTime) {
+        if (executionTime > 1000) {
+            externalApiLogger.warn(EXTERNAL_API_SLOW_RESPONSE.getMessage(),
+                    keyValue(EXECUTION_TIME.getKey(), executionTime),
+                    keyValue(SUCCESS.getKey(), true)
+            );
+        } else {
+            externalApiLogger.info(EXTERNAL_API_SUCCESS.getMessage(),
+                    keyValue(EXECUTION_TIME.getKey(), executionTime),
+                    keyValue(SUCCESS.getKey(), true));
+        }
+    }
+
+    @Override
+    public void logError(String message) {
+        externalApiLogger.warn(EXTERNAL_API_FAIL.getMessage(),
+                keyValue(ERROR_MESSAGE.getKey(), message),
+                keyValue(SUCCESS.getKey(), false));
+    }
+}
