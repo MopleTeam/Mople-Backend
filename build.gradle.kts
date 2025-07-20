a40cbf6b3ab79b7b3c54c6fa927c55a1c2a2dfdd
이대영 <kmdy7991@naver.com>
2025-07-20 12:19:08 +0900

    fix: 버전 카탈로그 방식으로 변환(gradle 권장)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,144 +1,73 @@
-import java.util.Properties
-
-val buildProps = Properties().apply {
-    file("gradle.properties")
-        .takeIf { it.exists() }
-        ?.inputStream()
-        ?.use(::load)
-}
-
-val projectGroup: String = buildProps.getProperty("projectGroup")
-val applicationVersion: String = buildProps.getProperty("applicationVersion")
-
-val springCloudVersion: String = buildProps.getProperty("springCloudVersion")
-
-val bouncyCastleVersion: String = buildProps.getProperty("bouncyCastleVersion")
-val jwtVersion: String = buildProps.getProperty("jwtVersion")
-val okHttpVersion: String = buildProps.getProperty("okHttpVersion")
-val awsVersion: String = buildProps.getProperty("awsVersion")
-val xmlBindVersion: String = buildProps.getProperty("xmlBindVersion")
-val persistenceVersion: String = buildProps.getProperty("persistenceVersion")
-val mapperVersion: String = buildProps.getProperty("mapperVersion")
-val lombokMapperVersion: String = buildProps.getProperty("lombokMapperVersion")
-val swaggerVersion: String = buildProps.getProperty("swaggerVersion")
-val queryDslVersion: String = buildProps.getProperty("queryDslVersion")
-val datasourceProxyVersion: String = buildProps.getProperty("datasourceProxyVersion")
-val logbackEncoderVersion: String = buildProps.getProperty("logbackEncoderVersion")
-val firebaseVersion: String = buildProps.getProperty("firebaseVersion")
-val dataFakerVersion: String = buildProps.getProperty("dataFakerVersion")
-val fixtureMonkeyVersion: String = buildProps.getProperty("fixtureMonkeyVersion")
-
 val mockitoAgent: Configuration by configurations.creating
 
 plugins {
-    id("java")
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
+    alias(libs.plugins.java)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
 }
 
-group = projectGroup
-version = applicationVersion
+group = libs.versions.project.group.get()
+version = libs.versions.application.version.get()
 
 java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
-    mavenCentral()
+    mavenCentral() // mokito configuration 으로 중복, mockito docs 권장 방식이라 유지
 }
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${libs.versions.spring.cloud.get()}")
+        mavenBom("io.awspring.cloud:spring-cloud-aws-dependencies:${libs.versions.aws.get()}")
     }
 }
 
 dependencies {
-    // spring
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-aop")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation(libs.bundles.spring.boot.web) // spring
 
-    // security
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-    implementation("org.bouncycastle:bcpkix-jdk18on:${bouncyCastleVersion}")
+    compileOnly(libs.lombok) // lombok
+    annotationProcessor(libs.lombok)
 
-    // jwt
-    implementation("io.jsonwebtoken:jjwt-api:${jwtVersion}")
-    implementation("io.jsonwebtoken:jjwt-impl:${jwtVersion}")
-    implementation("io.jsonwebtoken:jjwt-jackson:${jwtVersion}")
+    implementation(libs.bundles.spring.security) // secure
+    implementation(libs.bouncy.castle)
+    implementation(libs.bundles.jwt)
 
-    // ssr - link
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
+    implementation(libs.bundles.template) // template
 
-    // client
-    implementation("com.squareup.okhttp3:okhttp:${okHttpVersion}")
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation(libs.spring.cloud.starter.openfeign) // client
+    implementation(libs.okhttp)
 
-    // s3 - profile image
-    implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:${awsVersion}"))
-    implementation("io.awspring.cloud:spring-cloud-aws-starter-s3")
-    implementation("jakarta.xml.bind:jakarta.xml.bind-api:${xmlBindVersion}")
+    implementation(libs.spring.cloud.aws.starter.s3) // s3
+    implementation(libs.xml.bind.api)
 
-    // lombok
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
+    runtimeOnly(libs.postgresql) // db, orm
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.hypersistence.utils)
+    implementation(libs.querydsl.jpa)
+    annotationProcessor("${libs.querydsl.apt.get()}:jpa") // openfeign qclass 생성 문제 (:jpa 추가)
+    annotationProcessor(libs.jakarta.annotation.api)
+    annotationProcessor(libs.jakarta.persistence.api)
 
-    // db
-    runtimeOnly("org.postgresql:postgresql")
+    implementation(libs.mapstruct) // mapper
+    annotationProcessor(libs.mapstruct.processor)
+    implementation(libs.lombok.mapstruct.binding)
 
-    // json - db Mapping
-    implementation("io.hypersistence:hypersistence-utils-hibernate-63:${persistenceVersion}")
+    implementation(libs.springdoc.openapi) // docs
 
-    // object mapping
-    // binding library를 통해 lombok 순서 상관없이 사용가능
-    implementation("org.mapstruct:mapstruct:${mapperVersion}")
-    annotationProcessor("org.mapstruct:mapstruct-processor:${mapperVersion}")
-    implementation("org.projectlombok:lombok-mapstruct-binding:${lombokMapperVersion}")
+    annotationProcessor(libs.spring.boot.configuration.processor) // property assist
 
-    // swagger
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${swaggerVersion}")
+    implementation(libs.datasource.proxy) // Datasource Proxy (슬로우 쿼리 측정하기 위해)
 
-    // jpa
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation(libs.logstash.logback.encoder) // Logstash Logback Encoder (로그를 JSON 형태로 변환)
 
-    // querydsl/queryDsl -> openfeign/queryDsl
-    // query performence 고려 -> (mybatis, jooq)
-    implementation("io.github.openfeign.querydsl:querydsl-jpa:${queryDslVersion}")
-    // openfeign qclass 생성 문제 (:jpa 추가)
-    annotationProcessor("io.github.openfeign.querydsl:querydsl-apt:${queryDslVersion}:jpa")
+    implementation(libs.firebase.admin) // io.netty 4.1.115 미만 windows dos 공격 취약점 spring netty 4.1.115 이상 지원할 때 변경 - spring 3.4.0에서 해결
 
-    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
-    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+    implementation(libs.datafaker) // test dummy
 
-    // property assist tool
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-    // Datasource Proxy (슬로우 쿼리 측정하기 위해)
-    implementation("net.ttddyy:datasource-proxy:${datasourceProxyVersion}")
-
-    // Logstash Logback Encoder (로그를 JSON 형태로 변환)
-    implementation("net.logstash.logback:logstash-logback-encoder:${logbackEncoderVersion}")
-
-    // firebase
-    // io.netty 4.1.115 미만 windows dos 공격 취약점 spring netty 4.1.115 이상 지원할 때 변경 - spring 3.4.0에서 해결
-    implementation("com.google.firebase:firebase-admin:${firebaseVersion}")
-
-    // dummy data
-    implementation("net.datafaker:datafaker:${dataFakerVersion}")
-
-    // test
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:${fixtureMonkeyVersion}")
-
-    // Mockito
-    mockitoAgent(libs.mockito) { isTransitive = false }
+    testRuntimeOnly(libs.junit.platform.launcher) // test
+    testImplementation(libs.bundles.test)
     testImplementation(libs.mockito)
+    mockitoAgent(libs.mockito) { isTransitive = false }
 }
 
 tasks.withType<Test> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,9 @@
 rootProject.name = "mople"
 
 pluginManagement {
-    val springBootVersion:           String by settings
-    val dependencyManagementVersion: String by settings
-
-    resolutionStrategy {
-        eachPlugin {
-            when(requested.id.id) {
-                "org.springframework.boot"            -> useVersion(springBootVersion)
-                "io.spring.dependency-management"     -> useVersion(dependencyManagementVersion)
-            }
-        }
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
     }
 }
 
