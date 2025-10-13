8fdb8ffab497010b308522c484eac9f61bbced85
OhYeHyun <ohyhohyl@gmail.com>
2025-10-13 23:21:16 +0900

    feat: 캐시 무효화 커스텀 어노테이션 추가

---
+++ src/main/java/com/mople/core/annotation/cache/InvalidateCache.java
@@ -0,0 +1,13 @@
+package com.mople.core.annotation.cache;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InvalidateCache {
+    String cacheName();
+    String[] keys();
+}
