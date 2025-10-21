dea9d434a4c518e5e9b60d348086b32f8d5f1cbb
OhYeHyun <ohyhohyl@gmail.com>
2025-10-21 21:57:30 +0900

    Revert "Merge pull request #26 from MopleTeam/feat/async-event"
    This reverts commit 6c8228999798034fecd2f1d68cf59520c80abe12, reversing
    changes made to 96552db741f56d3fae68028e949ea10238203d21.

--- src/main/java/com/mople/outbox/service/OutboxPublisher.java
+++ src/main/java/com/mople/outbox/service/OutboxPublisher.java
@@ -1,7 +1,6 @@
 package com.mople.outbox.service;
 
 import com.mople.entity.event.OutboxEvent;
-import com.mople.global.logging.logger.BusinessLogicLogger;
 import com.mople.outbox.repository.OutboxEventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +8,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +15,6 @@ public class OutboxPublisher {
 
     private final OutboxProcessor processor;
     private final OutboxEventRepository outboxEventRepository;
-    private final BusinessLogicLogger logicLogger;
-    private final Executor outboxExecutor;
 
     @Value("${outbox.batch-size}")
     private int batchSize;
@@ -31,18 +26,8 @@ public class OutboxPublisher {
     public void publishBatch() {
         List<OutboxEvent> outboxEvents = outboxEventRepository.lockNextBatch(batchSize, leaseSec);
 
-        List<CompletableFuture<Void>> futures = outboxEvents.stream()
-                .map(event -> CompletableFuture.runAsync(
-                        () -> processor.processOne(event),
-                        outboxExecutor
-                ).exceptionally(ex -> {
-                    logicLogger.logError("OutboxEvent 처리 실패");
-                    return null;
-                }))
-                .toList();
-
-        CompletableFuture
-                .allOf(futures.toArray(new CompletableFuture[0]))
-                .join();
+        for (OutboxEvent event : outboxEvents) {
+            processor.processOne(event);
+        }
     }
 }
